9edb2939a37fcf13844b2ebd5aaf81ce511f0ae4
Ledia <ledialuli@icloud.com>
2024-02-29 11:45:08 +0000

    Adding message server and simple server with secrets removed.

--- cloud_deployment/codebases/simple_server/app.py
+++ cloud_deployment/codebases/simple_server/app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(
